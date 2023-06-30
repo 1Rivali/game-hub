84d0b65eaf3cddba298672671ec47c7414a5e347
1Rivali <fadyjonsina@gmail.com>
2023-06-30 17:13:17 +0300

    Handle Games With No Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,11 @@
+import noImage from '../assets/no_image_to_show_.webp';
+
 const getCropedImageUrl = (
   url: string,
   width: number = 600,
   height: number = 400
 ) => {
-  if (!url) return '';
+  if (!url) return noImage;
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + `crop/${width}/${height}/` + url.slice(index);
